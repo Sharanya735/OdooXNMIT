fad27cf5a120183d79fa8d4d2f5f1422255b7676
Nisha K <nishak2125@gmail.com>
2025-10-03 18:04:50 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,8 +0,0 @@
-// src/main.jsx
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css"; // if not present, create an empty file or remove this line
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
